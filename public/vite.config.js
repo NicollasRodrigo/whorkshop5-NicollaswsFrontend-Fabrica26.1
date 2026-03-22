9f96e3c1e9981e019690e70a055a417deb2babe5
nicollas.rodrigoo <nicollas.rodrigoo@gmail.com>
2026-03-22 12:27:21 -0300

    Add files via upload

---
+++ public/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base:"https://github.com/NicollasRodrigo/Nicollas_RodrigowsFrontend-Fabrica26.1.giprj_vYMr9g0C7F8qjdSUEYpRnNpkUA8v",
+  plugins: [react()],
+})
